19536d383e32c5951be0a0b3daef4934b1bb974e
Gemma C <gmacastro84@gmail.com>
2017-02-13 14:58:04 +0100

    String Calculator. Add method that sum, at most, two numbers

---
+++ KataStringCalculator/KataStringCalculator/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace KataStringCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringCalculator calculator = new StringCalculator();
+
+            Console.WriteLine("No numbers:");
+            Console.WriteLine("Result: {0}", calculator.Add(""));
+            Console.WriteLine("Two numbers:");
+            Console.WriteLine("Result: {0}", calculator.Add("1,3"));
+            Console.WriteLine("Three numbers:");
+            Console.WriteLine("Result: {0}", calculator.Add("1,3,4"));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ KataStringCalculator/KataStringCalculatorTests/StringCalculatorTests.cs
@@ -0,0 +1,60 @@
+﻿using KataStringCalculator;
+using Microsoft.VisualStudio.TestTools.UITesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace KataStringCalculatorTests
+{
+    /// <summary>
+    /// Summary description for CodedUITest1
+    /// </summary>
+    [CodedUITest]
+    public class StringCalculatorTests
+    {
+        private StringCalculator calculator; 
+        public StringCalculatorTests()
+        {
+            calculator = new StringCalculator();
+        }
+
+        [TestMethod]
+        public void AddTest_EmptyString()
+        {
+            string numberString = "";
+
+            var result = calculator.Add(numberString);
+
+            Assert.IsTrue(result == 0);
+        }
+
+        [TestMethod]
+        public void AddTest_OneNumber()
+        {
+            string numberString = "7";
+
+            var result = calculator.Add(numberString);
+
+            Assert.IsTrue(result == 7);
+        }
+
+        [TestMethod]
+        public void AddTest_TwoNumbers()
+        { 
+            string numberString = "1,2";
+
+            var result = calculator.Add(numberString);
+
+            Assert.IsTrue(result == 3);
+            
+        }
+        // There is no need to test for invalid inputs 
+        //[ExpectedException(typeof(FormatException))]
+        //[TestMethod]
+        //public void AddTest_InvalidFormat()
+        //{
+        //    string numberString = "1-2";
+
+        //    var result = calculator.Add(numberString);
+        //}   
+    }
+}
